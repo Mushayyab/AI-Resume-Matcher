3f257a113c47ec787eb555a701a8d7c1de0b64ac
Mushayyab Mujahidmasud Lakdawala <143339918+Mushayyab@users.noreply.github.com>
2026-02-03 16:55:18 -0500

    Completed core matching logic and UI

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AI-Resume-Matcher
 A Python-based NLP tool that analyzes Job Descriptions against PDF Resumes to calculate a match percentage and identify missing keywords.
+
+## Features (e.g., PDF Parsing, Keyword Extraction)
+
+## Tech Stack (Python, Streamlit, Scikit-Learn)
+
+## How to Run (The pip install and streamlit run commands)
--- app.py
+++ app.py
@@ -1,3 +1,33 @@
+from pypdf import PdfReader # type: ignore
 import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def extract_text_from_pdf(file):
+    pdf = PdfReader(file)
+    text = ""
+    for page in pdf.pages:
+        text += page.extract_text()
+    return text
+
+def calculate_similarity(resume_text, job_desc):
+    content = [resume_text, job_desc]
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(content)
+    similarity = cosine_similarity(count_matrix)
+    return similarity[0][1] * 100
+
 st.title("AI Resume Matcher")
-st.write("Ready to match some resumes!")
+st.write("This app helps you check if your Resume matches a Job Description.")
+
+uploaded_file = st.file_uploader("Upload your Resume", type="pdf")
+job_description = st.text_area("Paste your Job Description here")
+
+if st.button("Compare Now"):
+    if uploaded_file is not None and job_description:
+        resume_text = extract_text_from_pdf(uploaded_file)
+        score = calculate_similarity(resume_text,job_description)
+        st.subheader(f"Match Score: {score:.2f}%")
+    else:
+        st.error("Please upload a resume and paste a job description first!")
+
